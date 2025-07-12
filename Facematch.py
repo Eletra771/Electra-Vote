3d72ebad232d376b0cc7b935de8c7f913dbe383d
ABHISEK KUNDU <abhisekkundu86@gmail.com>
2025-07-12 09:56:12 +0530

    only face matching part

---
+++ Facematch.py
@@ -0,0 +1,28 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": [],
+      "authorship_tag": "ABX9TyM8Kt1CEoZEZ66kOmDrUi91"
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "code",
+      "execution_count": null,
+      "metadata": {
+        "id": "D_W9IrH24Ncb"
+      },
+      "outputs": [],
+      "source": []
+    }
+  ]
+}
